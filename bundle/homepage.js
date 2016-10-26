6add4caa1d9552f890e50bedfeff5c224ba3016c
yu xuan <yu xuan>
2016-10-26 11:12:26 +0800

    original commit

--- bundle/homepage.js
+++ bundle/homepage.js
@@ -44,7 +44,14 @@
 /* 0 */
 /***/ function(module, exports) {
 
-
+	// (function() {
+	//     var title = document.querySelector('#title');
+	//     var title_pos = 0;
+	//     title.addEventListener('mousemove',function(e){
+	//         title_pos += 50;
+	//         title.style.transform = "translateY("+title_pos+")";
+	//     })
+	// })()
 
 /***/ }
 /******/ ]);
