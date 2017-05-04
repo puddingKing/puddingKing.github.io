e59095f68ecc4056c3a11895c654163d43f9d6bb
puddingKing <1033867541@qq.com>
2017-05-04 15:38:49 +0800

    'test'

--- bundle/homepage.js
+++ bundle/homepage.js
@@ -44,14 +44,7 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	// (function() {
-	//     var title = document.querySelector('#title');
-	//     var title_pos = 0;
-	//     title.addEventListener('mousemove',function(e){
-	//         title_pos += 50;
-	//         title.style.transform = "translateY("+title_pos+")";
-	//     })
-	// })()
+
 
 /***/ }
 /******/ ]);
